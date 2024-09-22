b92938dc7dee4047d6ba1fb74d6cdc0895c233da
Victoria Stratulat <victoria.stratulat@gmail.com>
2024-09-22 23:17:15 +0300

    feat: add next and previous buttons

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,7 +1,22 @@
 const PLAY = document.getElementById("play-pause");
 const AUDIO = document.getElementById("music");
+const NEXT = document.getElementById("next");
+const PREVIOUS = document.getElementById("previous");
+const SONGS = ["./assets/audio/beyonce.mp3", "./assets/audio/dontstartnow.mp3"];
 
 let isPlay = false;
+let i = 0;
+
+playAudio = () => {
+  AUDIO.src = SONGS[i];
+  if (!isPlay) {
+    AUDIO.play();
+    isPlay = true;
+  } else {
+    AUDIO.pause();
+    isPlay = false;
+  }
+};
 
 PLAY.addEventListener("click", () => {
   console.log("clicked");
@@ -13,12 +28,28 @@ PLAY.addEventListener("click", () => {
   playAudio();
 });
 
-playAudio = () => {
-  if (!isPlay) {
-    AUDIO.play();
-    isPlay = true;
+NEXT.addEventListener("click", () => {
+  AUDIO.pause();
+  if (i < SONGS.length - 1) {
+    i++;
   } else {
-    AUDIO.pause();
-    isPlay = false;
+    i = 0;
   }
-};
+  AUDIO.src = SONGS[i];
+  console.log("i = ", i);
+  AUDIO.play();
+  isPlay = true;
+});
+
+PREVIOUS.addEventListener("click", () => {
+  AUDIO.pause();
+  if (i - 1 >= 0) {
+    --i;
+  } else {
+    i = SONGS.length - 1;
+  }
+  AUDIO.src = SONGS[i];
+  console.log("i = ", i);
+  AUDIO.play();
+  isPlay = true;
+});
